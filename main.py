94fae3f42ab3c90da609c7e376f5c09a88ceb965
Kacper Zieliński <zielinski_kacper@outlook.com>
2024-11-16 00:24:38 +0100

    Add new_game function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from os import system
+
 def menu():
     print("Welcome to the TERPGMINAL!")
     print("1 - New game")
@@ -7,7 +9,8 @@ def menu():
     choice = input("Select option (1-3): ")
     match choice:
         case '1':
-            print("Start new game")
+            system('clear')
+            new_game()
         case '2':
             print("Load game")
         case '3':
@@ -15,5 +18,18 @@ def menu():
         case _:
             print("Invalid choice!")
 
-
+def new_game():
+    character_data = open("character_data.txt", "w+")
+    character_name = input(("What's your name?: "))
+    character_data.write(character_name + ';')
+    system('clear')
+    print("1 - Warrior")
+    print("2 - Wizard")
+    choice = input("Choose your class: ")
+    match choice:
+        case '1':
+            character_class = "Warrior"
+        case '2':
+            character_class = "Wizard"
+    character_data.write(character_class + ';')
 menu()
